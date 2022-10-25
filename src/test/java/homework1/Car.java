e31bc3d335235846e7b558fe17af6935f726ab8a
TIMOXA <timofejvasiljev@gmail.com>
2022-10-25 16:06:57 +0300

    Starting home work test, using GETTERS/SETTERS

---
+++ src/test/java/homework1/Car.java
@@ -0,0 +1,51 @@
+package homework1;
+
+public class Car {
+    private int nummber;
+    private String firma;
+    private double totalPrice;
+    private String color;
+    private int speed;
+
+    //----------GETTER/SETTER------------
+
+    public int getNummber() {
+        return nummber;
+    }
+
+    public void setNummber(int nummber) {
+        this.nummber = nummber;
+    }
+
+    public String getFirma() {
+        return firma;
+    }
+
+    public void setFirma(String firma) {
+        this.firma = firma;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
