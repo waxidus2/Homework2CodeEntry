50d3a943ea9231ed97badc91d1b3039afb7946f6
TIMOXA <timofejvasiljev@gmail.com>
2022-11-07 16:00:37 +0200

    Xpath search list.

---
+++ src/test/java/finishedhomework/XpathList.java
@@ -0,0 +1,19 @@
+package finishedhomework;
+
+import org.openqa.selenium.By;
+
+public class XpathList {
+
+   private final By DISCOVER_CARS_IMG = By.xpath(".//img[@src='https://www.discovercars.com/assets/desktop/img/svg/discovercars-blue.svg']");
+   private final By CHOOSE_CURRENCY = By.xpath (".//a[@id='currentCurrency']");
+   private final By LOGIN_MODAL_POINTER = By.xpath (".//a[@data-label='Navigation']");
+   private final By HOME_CAPTION = By.xpath("..//div[@class='home-caption']");
+   private final By DATE_LEFT = By.xpath(".//div[@class='sb-group dates left']");
+   private final By RESIDENCE_TEXT = By.xpath (".//span[@class='sb-residence-text']");
+   private final By SEARCH_BUTTON = By.xpath(".//button[@class='button button-green button-lg submit prevent-loading trigger-gtm-sb gtm-track-fix']");
+   private final By WIDGET_REVIEW = By.xpath(".//div[4][@class='tp-widget-review']");
+   private final By WE_COMPARE = By.xpath (".//div[@class='row is-table-row']");
+
+
+
+}
